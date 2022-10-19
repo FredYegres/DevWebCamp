d9fe7f3bc0ae622d75c2ad7311a689da1a7d7c23
Fred Yegres <yegrescisneros@gmail.com>
2022-10-19 00:14:27 -0400

    Cambio en clase email

--- classes/Email.php
+++ classes/Email.php
@@ -20,59 +20,55 @@ class Email {
 
     public function enviarConfirmacion() {
 
-         // create a new object
-         $mail = new PHPMailer();
-         $mail->isSMTP();
-         $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-         $mail->Host = $_ENV['EMAIL_HOST'];
-         $mail->SMTPAuth = true;
-         $mail->Port = $_ENV['EMAIL_PORT'];
-         $mail->Username = $_ENV['EMAIL_USER'];
-         $mail->Password = $_ENV['EMAIL_PASS'];
-     
-         $mail->setFrom($_ENV['EMAIL_USER'], 'Cuentas DevWebCamp');
-         $mail->addReplyTo('cuentas@devwebcamp.com', 'Cuentas DevWebCamp');
-         $mail->addAddress($this->email, $this->nombre);
-         $mail->Subject = 'Confirma tu Cuenta';
+        // create a new object
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Port = $_ENV['EMAIL_PORT'];
+        $mail->Username = $_ENV['EMAIL_USER'];
+        $mail->Password = $_ENV['EMAIL_PASS'];
+    
+        $mail->setFrom($_ENV['EMAIL_USER'], 'Cuentas DevWebCamp');
+        $mail->addReplyTo('cuentas@devwebcamp.com', 'Cuentas DevWebCamp');
+        $mail->addAddress($this->email, $this->nombre);
+        $mail->Subject = 'Confirma tu Cuenta';
 
-         // Set HTML
-         $mail->isHTML(TRUE);
-         $mail->CharSet = 'UTF-8';
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
 
-         $contenido = '<html>';
-         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has Registrado Correctamente tu cuenta en DevWebCamp; pero es necesario confirmarla</p>";
-         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";       
-         $contenido .= "<p>Si tu no creaste esta cuenta; puedes ignorar el mensaje</p>";
-         $contenido .= '</html>';
-         $mail->Body = $contenido;
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has Registrado Correctamente tu cuenta en DevWebCamp; pero es necesario confirmarla</p>";
+        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";       
+        $contenido .= "<p>Si tu no creaste esta cuenta; puedes ignorar el mensaje</p>";
+        $contenido .= '</html>';
+        $mail->Body = $contenido;
 
-         //Enviar el mail
+        //Enviar el mail
         $mail->send();
 
     }
 
     public function enviarInstrucciones() {
 
-         // create a new object
-         $mail = new PHPMailer();
-         $mail->isSMTP();
-         $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-         $mail->Host = $_ENV['EMAIL_HOST'];
-         $mail->SMTPAuth = true;
-         $mail->Port = $_ENV['EMAIL_PORT'];
-         $mail->Username = $_ENV['EMAIL_USER'];
-         $mail->Password = $_ENV['EMAIL_PASS'];
-     
-         $mail->setFrom($_ENV['EMAIL_USER'], 'Cuentas DevWebCamp');
-         $mail->addReplyTo('cuentas@devwebcamp.com', 'Cuentas DevWebCamp');
-         $mail->addAddress($this->email, $this->nombre);
-         $mail->Subject = 'Confirma tu Cuenta';
+        // create a new object
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Port = $_ENV['EMAIL_PORT'];
+        $mail->Username = $_ENV['EMAIL_USER'];
+        $mail->Password = $_ENV['EMAIL_PASS'];
 
-         // Set HTML
-         $mail->isHTML(TRUE);
-         $mail->CharSet = 'UTF-8';
+        $mail->setFrom($_ENV['EMAIL_USER'], 'Cuentas DevWebCamp');
+        $mail->addReplyTo('cuentas@devwebcamp.com', 'Cuentas DevWebCamp');
+        $mail->addAddress($this->email, $this->nombre);
+        $mail->Subject = 'Confirma tu Cuenta';
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
@@ -81,7 +77,7 @@ class Email {
         $contenido .= '</html>';
         $mail->Body = $contenido;
 
-         //Enviar el mail
+        //Enviar el mail
         $mail->send();
     }
 }
